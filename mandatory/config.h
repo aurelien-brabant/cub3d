cbf1399e10184b7160390722289013b4e900f71d
abrabant <abrabant@student.42.fr>
2021-02-28 00:50:20 +0100

    Remove comment in config.h

--- mandatory/config.h
+++ mandatory/config.h
@@ -6,7 +6,7 @@
 /*   By: abrabant <abrabant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/22 21:54:03 by abrabant          #+#    #+#             */
-/*   Updated: 2021/02/25 23:11:24 by abrabant         ###   ########.fr       */
+/*   Updated: 2021/02/28 00:50:04 by abrabant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@
 **
 ** Anything put in this header is intended to be modified
 ** modified by the user.
-**
-** DON'T FORGET to run 'make reconfig' when changes have been made.
 */
 
 # define FOV_ANGLE 60
